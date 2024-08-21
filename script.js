15bf372be7fba4657b2e3fd0f86f23cea20bee06
Chinwuko John Obielue <147950784+ObicoJohn@users.noreply.github.com>
2024-08-21 17:18:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// $(window).on('load', function () {
-//     $(".preloader").fadeOut(800);
-//     $(".home-container").fadeIn(800);
-// });
+$(window).on('load', function () {
+    $(".preloader").fadeOut(800);
+    $(".home-container").fadeIn(800);
+});
 
 //farming button
 
